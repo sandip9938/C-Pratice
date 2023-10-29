5e0e2ddb0cc5843e02d33b3c581b1cadf08adbc8
Sandip Panda <115527258+sandip9938@users.noreply.github.com>
2023-10-29 23:01:08 +0530

    Class 1,2,3

---
+++ Class1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+// cout<<"Hello "<<endl;
+// cout<<"Hello sir "<<endl;
+int x=19;
+cout<<x+20<<endl; 
+}
+
---
+++ Class2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // Updation
+    int x = 19;
+    cout << x << endl;
+    x = 12;
+    cout << x << endl;
+    // Opreator
+    //Arithmatic Opreator 
+    int m=10;
+    int k=12;
+    cout<<m+k<<endl;
+    cout<<m-k<<endl;
+    cout<<m*k<<endl;
+    cout<<m/k<<endl;
+    cout<<m%k<<endl;
+}
